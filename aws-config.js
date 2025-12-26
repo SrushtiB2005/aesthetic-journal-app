65c1d2c1c7910d619b914318b345c7f73b6aa7fb
SrushtiB2005 <bhosale.srushti2005@gmail.com>
2025-12-26 23:26:17 +0530

    Refactor AWS configuration for Cognito authentication

--- aws-config.js
+++ aws-config.js
@@ -1,7 +1,21 @@
-Amplify.configure({
-  Auth: {
-    region: "ap-south-1",
-    userPoolId: "ap-south-1_hjXm4aBOK",
-    userPoolWebClientId: "5ul13qn0qutpkd4u44ngso4k59"
-  }
+import { UserManager } from "oidc-client-ts";
+
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_hjXm4aBOK",
+    client_id: "5ul13qn0qutpkd4u44ngso4k59",
+    redirect_uri: "https://main.d1db5aofv9kad3.amplifyapp.com/",
+    response_type: "code",
+    scope: "email openid phone"
+};
+
+// create a UserManager instance
+export const userManager = new UserManager({
+    ...cognitoAuthConfig,
 });
+
+export async function signOutRedirect () {
+    const clientId = "5ul13qn0qutpkd4u44ngso4k59";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://ap-south-1hjxm4abok.auth.ap-south-1.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+};
