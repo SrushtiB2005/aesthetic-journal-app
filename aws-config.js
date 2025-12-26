2d3520e7003cc31c36ee9754e2d90d48b33fd4d3
SrushtiB2005 <bhosale.srushti2005@gmail.com>
2025-12-26 21:30:22 +0530

    Fix Amplify configuration syntax in aws-config.js

--- aws-config.js
+++ aws-config.js
@@ -1,10 +1,7 @@
-const { Amplify } = aws_amplify;
-
-Amplify.configure({
+aws_amplify.Amplify.configure({
   Auth: {
     region: "ap-south-1",
     userPoolId: "ap-south-1_hjXm4aBOK",
     userPoolWebClientId: "5ul13qn0qutpkd4u44ngso4k59"
   }
 });
-
